45a8c7b59996a81f2e959cd31e85aebb1b299996
showkw <showkw@163.com>
2018-07-09 02:29:23 +0800

    some change

--- src/Flow/FlowManager.php
+++ src/Flow/FlowManager.php
@@ -1,17 +1,7 @@
 <?php
 
 // +----------------------------------------------------------------------+
-// | The Zs Framework.                                                |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2016-2017 Shanxi Benchu Network Technology Co,.Ltd     |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the Apache license,    |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.apache.org/licenses/LICENSE-2.0.html                      |
-// +----------------------------------------------------------------------+
-// | Author: TwilRoad <heshudong@ibenchu.com>                             |
-// |         Seven Du <shiweidu@outlook.com>                              |
+// | The Zs Framework.                                                    |
 // +----------------------------------------------------------------------+
 
 namespace Zs\Foundation\Flow;
